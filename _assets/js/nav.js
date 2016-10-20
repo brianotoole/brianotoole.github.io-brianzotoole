e1663c87e3330d5bfecb730b198447690bc082f0
Brian <robbie@2nldb7-2.local>
2016-10-19 20:50:27 -0400

    setup js asset pipeline + nav

---
+++ _assets/js/nav.js
@@ -0,0 +1,28 @@
+// Nav.js
+// Slide out navigation
+// on click of #js-nav el
+$(document).ready(function() {
+  var trigger = $('.hamburger'),
+    overlay = $('.overlay'),
+    isClosed = false;
+  trigger.click(function() {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+  }
+  $('[data-toggle="offcanvas"]').click(function() {
+    $('#js-nav').toggleClass('toggled');
+  });
+});
