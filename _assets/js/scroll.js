e09c0ba22191b93350eade2273807c41c8c8f9fa
Brian <robbie@iMac.local>
2016-12-17 16:39:19 -0500

    adjusted smooth scroll

--- _assets/js/scroll.js
+++ _assets/js/scroll.js
@@ -1,7 +1,15 @@
 // scroll on click of anchor link (a href="#")
 $(function() {
-	$('a[href*=#]').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-	});
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
 });
